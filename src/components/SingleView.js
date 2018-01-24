eb247e0a33e2cbdbba543e0800609c407faeee06
VictorAtanasov <viktor_atanasovv@yahoo.com>
2018-01-24 23:52:23 +0200

    single view component nested data formating function added

--- src/components/SingleView.js
+++ src/components/SingleView.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import {FontawesomeSpinner} from '../common/FontawesomeSpinner';
 import {Link} from 'react-router-dom';
 
 
 export const SingleView = (props) => {
 
+    function extractData(arr, title){
+        if(title === 'residents'){
+            title = 'resident'
+        } else if(title === 'characters'){
+            title = 'character'
+        } else if(title === 'pilots'){
+            title = 'pilot'
+        }
+        let counter = 0;
+        let nestedData = [];
+        for(let y of arr){
+            nestedData.push(
+                <p key={counter += 1}>
+                    {title + ' ' + y.match(/\d+/)[0]}
+                </p>
+            )
+        }
+        return nestedData
+    }
+
     function loadedData(){
-        let keys = Object.keys(props.data)
-        console.log(keys)   
-        let loadedData = []; 
+        let keys = Object.keys(props.data);
+        let loadedData = [];
         for(let i = 0; i < keys.length-3; i += 1){
-            //console.log(typeof props.data[keys[i]])
             if(typeof props.data[keys[i]] === 'object'){
-                console.log(props.data[keys[i]])
+                loadedData.push(
+                    <div key={i}>
+                        <p>{keys[i]}</p>
+                        {extractData(props.data[keys[i]], keys[i])}
+                        <div className="detail-border"></div>
+                    </div>
+                );
             }else{
                 loadedData.push(
                     <div key={i}>
                         <p>{keys[i]}</p>
                         <p>{props.data[keys[i]]}</p>
+                        <div className="detail-border"></div>
                     </div>
                 )
             }
